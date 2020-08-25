a4b4e6407e4feb6f3ccc1270777e4c657488933f
santhosh <santhoshkadridevadiga@gmail.com>
2020-08-25 17:15:43 +0530

    added basic logger and exception handler

--- core/xtractor.py
+++ core/xtractor.py
@@ -1,12 +1,14 @@
+import settings
+
 from dataclasses import dataclass
 from argparse import ArgumentParser
 from core.http_service import HttpRequest
 from core.parser_service import HtmlParser
 from core.xception import XtendedExcpetion
-
+from core.db_service import AlchemyService
+from core.logger import Logger
 from requests.exceptions import ProxyError
-import settings
-from core.xception import MaxRetryException
+
 
 @dataclass
 class BaseExtractor:
@@ -14,6 +16,8 @@ class BaseExtractor:
     request = HttpRequest()
     arg_parser = ArgumentParser('scraper')
     cmd_args = None
+    db = AlchemyService()
+    log = None
 
     def execute(self):
         raise XtendedExcpetion('Method execute not implemented')
@@ -28,11 +32,10 @@ class BaseExtractor:
         self.arg_parser = self.init_xtarctor()
 
     def run(self):
-        self.add_cmd_argument(self.arg_parser)
-        self.cmd_args = self.arg_parser.parse_args()
-        self.execute()
-
-
-
-
-
+        self.log = Logger(self.__class__.__name__)
+        try:
+            self.add_cmd_argument(self.arg_parser)
+            self.cmd_args = self.arg_parser.parse_args()
+            self.execute()
+        except Exception as e:
+            self.log.error(str(e))
